e6a7406605b12b509ded8f6b0017379d82be13aa
Williandiomario <williandiomario@gmail.com>
2024-08-04 03:11:11 -0300

    Update script.js

--- script.js
+++ script.js
@@ -9,8 +9,12 @@ function toggleMode() {
   if (html.classList.contains("light")) {
     // se estiver light mode, adicionar a imagem light
     img.setAttribute("src", "./assets/avatar-light.png")
+    img.setAttribute("alt", "Imagem de um leão")
+    img.setAttribute("title", "Imagem de um Leão")
   } else {
     // se estiver sem light mode, manter a imagem noturno
     img.setAttribute("src", "./assets/Avatar meu.png")
+    img.setAttribute("alt", "Imagem de Willian")
+    img.setAttribute("title", "Imagem de Willian")
   }
 }
